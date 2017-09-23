89b25d06b5647b23b34ae09af0734d5d86fde941
Abid Rakhmansyah <bidnth@gmail.com>
2017-09-23 12:14:47 +0700

    rollback index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include './partials/head.php' ?>
+
+<div class="container font-fwd">
+    <div class="row">
+        <div class="col-xs-6 col-xs-offset-3">
+            <div class="text-center">
+                <h1 class="font-signature">FISHMAN</h1>
+                <p class="lead">FWD Insurance: Simulasi Harapan Masa Depan</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php include './partials/foot.php' ?>
